b5231012f87532ccdfe1dafe227462c7078761b0
sanazniamati <sanaz.niamati1991@gmail.com>
2022-06-22 10:34:08 +0430

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Stage, Layer, Circle } from "react-konva";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight} draggable>
+      <Layer>
+        <Circle
+          x={150}
+          y={100}
+          radius={70}
+          fill={"red"}
+          stroke={"black"}
+          strokeWidth={4}
+        />
+      </Layer>
+    </Stage>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
